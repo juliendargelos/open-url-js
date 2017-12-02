808404f86255b6faa878795645eba94918988ce3
Julien Dargelos <julien.dargelos@me.com>
2017-12-02 21:10:27 +0100

    Logic

---
+++ README.md
@@ -0,0 +1,81 @@
+# Open URL JS 🔍
+
+Simply manage urls.
+
+## Install
+
+```
+npm install open-url-js
+```
+
+## Usage
+
+```javascript
+```
+
+<a name="Url"></a>
+
+## Url
+**Kind**: global class  
+
+* [Url](#Url)
+    * [new Url(...string)](#new_Url_new)
+    * [.string](#Url+string) : <code>string</code>
+    * [.protocol](#Url+protocol) : <code>string</code>
+    * [.host](#Url+host) : <code>string</code>
+    * [.hash](#Url+hash) : <code>string</code>
+    * [.path](#Url+path) : <code>Pathname</code>
+    * [.parameters](#Url+parameters) : <code>Parameters</code>
+    * [.toString()](#Url+toString) ⇒ <code>string</code>
+
+<a name="new_Url_new"></a>
+
+### new Url(...string)
+Create a [Url](#Url) object.
+
+
+| Param | Type | Description |
+| --- | --- | --- |
+| ...string | <code>Object</code> | Same value as [string](#Url+string). |
+
+<a name="Url+string"></a>
+
+### url.string : <code>string</code>
+A string corresponding to the url. When set, it will be parsed.
+
+**Kind**: instance property of [<code>Url</code>](#Url)  
+<a name="Url+protocol"></a>
+
+### url.protocol : <code>string</code>
+The protocol of the url.
+
+**Kind**: instance property of [<code>Url</code>](#Url)  
+<a name="Url+host"></a>
+
+### url.host : <code>string</code>
+The host of the url.
+
+**Kind**: instance property of [<code>Url</code>](#Url)  
+<a name="Url+hash"></a>
+
+### url.hash : <code>string</code>
+The hash of the url.
+
+**Kind**: instance property of [<code>Url</code>](#Url)  
+<a name="Url+path"></a>
+
+### url.path : <code>Pathname</code>
+The path of the url.
+
+**Kind**: instance property of [<code>Url</code>](#Url)  
+<a name="Url+parameters"></a>
+
+### url.parameters : <code>Parameters</code>
+The parameters of the url.
+
+**Kind**: instance property of [<code>Url</code>](#Url)  
+<a name="Url+toString"></a>
+
+### url.toString() ⇒ <code>string</code>
+**Kind**: instance method of [<code>Url</code>](#Url)  
+**Returns**: <code>string</code> - Value of [string](#Url+string).  
--- index.js
+++ index.js
@@ -1,10 +1,125 @@
-if(typeof require !== 'function') require = function() {};
-if(!Pathname) var Pathname = require('pathname-js');
-if(!Parameters) var Parameters = require('parameters-js');
-
 class Url {
+  static empty(value) {
+    return [undefined, null].includes(value);
+  }
+
+  /**
+   * Create a {@link Url} object.
+   * @param {...{(string)}} string Same value as {@link Url#string}.
+   */
   constructor(string) {
-    this.parameters = new Parameters();
-    this.pathname = new Pathname();
+    try {
+      if(typeof require !== 'function') require = function() {};
+    }
+    catch(e) {
+      var require = function() {};
+    }
+    if(!Pathname) Pathname = require('pathname-js');
+    if(!Parameters) Parameters = require('parameters-js');
+
+    this._protocol = '';
+    this._host = '';
+    this._hash = '';
+    this._path = new Pathname();
+    this._parameters = new Parameters();
+
+    this.string = string;
+  }
+
+  /**
+   * A string corresponding to the url. When set, it will be parsed.
+   * @type {string}
+   */
+  get string() {
+    return (
+      (this.protocol ? this.protocol + ':' : '') +
+      (this.host ? '//' + this.host : '') +
+      (this.host && !this.path.root ? '/' : '') +
+       this.path +
+      (this.path.length !== 0 && (this.hash || this.parameters.any) ? '/' : '') +
+      (this.hash ? '#' + this.hash : '') +
+      (this.parameters.any ? '?' + this.parameters : '')
+    );
+  }
+
+  set string(v) {
+    var match = (v + '').match(/^(?:([^:]*):\/\/)?([^\/\?#]*)([^\?#]*)(?:#([^?]*))?(?:\?(.*))?$/);
+    console.log(match);
+
+    if(match === null) match = [];
+
+    this.protocol = match[1];
+    this.host = match[2];
+    this.hash = match[3];
+    this.path = match[4];
+    this.parameters = match[5];
+  }
+
+  /**
+   * The protocol of the url.
+   * @type {string?}
+   */
+  get protocol() {
+    return this._protocol;
+  }
+
+  set protocol(v) {
+    this._protocol = this.constructor.empty(v) ? '' : v;
+  }
+
+  /**
+   * The host of the url.
+   * @type {string?}
+   */
+  get host() {
+    return this._host;
+  }
+
+  set host(v) {
+    this._host = this.constructor.empty(v) ? '' : v;
+  }
+
+  /**
+   * The hash of the url.
+   * @type {string?}
+   */
+  get hash() {
+    return this._hash;
+  }
+
+  set hash(v) {
+    this._hash = this.constructor.empty(v) ? '' : v;
+  }
+
+  /**
+   * The path of the url.
+   * @type {Pathname}
+   */
+  get path() {
+    return this._path;
+  }
+
+  set path(v) {
+    this.path.clear().concat(this.constructor.empty(v) ? '' : v);
+  }
+
+  /**
+   * The parameters of the url.
+   * @type {Parameters}
+   */
+  get parameters() {
+    return this._parameters;
+  }
+
+  set parameters(v) {
+    if(typeof v === 'string') this.parameters.string = this.constructor.empty(v) ? '' : v;
+    else this.parameters.clear().set(v);
+  }
+
+  /**
+   * @returns {string} Value of {@link Url#string}.
+   */
+  toString() {
+    return this.string;
   }
 }

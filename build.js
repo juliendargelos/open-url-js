3aaef111de449a36afbd9ade417abcbb2a316f83
Julien Dargelos <julien.dargelos@me.com>
2017-12-03 22:05:54 +0100

    Improved build

---
+++ build.js
@@ -0,0 +1,14 @@
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+exec('browserify index.js > dist/index.js');
+
+fs.readFile('index.js', 'utf8', function(error, data) {
+  if(error) throw error;
+
+  data = data.replace(/(module\.exports\s*=\s*|var\s+[^\s]+\s*=require\(['"][^'"]+['"]\);\n+)/g, '');
+
+  fs.writeFile('dist/open-url.js', data, function(error) {
+    if(error) throw error;
+  });
+});

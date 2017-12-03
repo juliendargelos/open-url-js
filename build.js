c76d198292867bdb880a76f38b0a2487708a4ec2
Julien Dargelos <julien.dargelos@me.com>
2017-12-03 23:57:20 +0100

    Fixed build.

--- build.js
+++ build.js
@@ -1,14 +1,15 @@
-var exec = require('child_process').exec;
 var fs = require('fs');
 
-exec('browserify index.js > dist/index.js');
-
-fs.readFile('index.js', 'utf8', function(error, data) {
-  if(error) throw error;
-
-  data = data.replace(/(module\.exports\s*=\s*|var\s+[^\s]+\s*=\s*require\(['"][^'"]+['"]\);?\n+)/g, '');
-
-  fs.writeFile('dist/open-url.js', data, function(error) {
+var clean = function(file, destination) {
+  fs.readFile(file, 'utf8', function(error, data) {
     if(error) throw error;
+
+    data = data.replace(/(module\.exports\s*=\s*|var\s+[^\s]+\s*=\s*require\(['"][^'"]+['"]\);?\n+)/g, '');
+
+    fs.writeFile(destination, data, function(error) {
+      if(error) throw error;
+    });
   });
-});
+};
+
+clean('index.js', 'open-url.js');
